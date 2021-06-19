92784b06ea23574586ce03b0ae086dcb101581a7
Leon Wilzer <leon.wilzer@protonmail.com>
2021-06-19 23:27:36 +0200

    debug

--- src/manage.role.js
+++ src/manage.role.js
@@ -7,19 +7,19 @@ var manageRole = {
 
         if(Object.keys(totalCreeps).length<10 && !Game.spawns['Spawn1'].spawning && Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', { dryRun: true })==0){
             // Keeps Harvesters at 50%
-            if(Object.keys(totalCreeps).length==0 || (harvesters.length / Object.keys(totalCreeps).length)<0.5) {
-                var newname = 'harvester' + Game.time;
+            if(Object.keys(totalCreeps).length==0 || (harvesters.length / Object.keys(totalCreeps).length)<=0.5) {
+                var newName = 'harvester' + Game.time;
                 console.log('Spawning new Harvester: ' + newname);
                 Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
                 {memory: {role: 'harvester'}});
             } // Keeps Builders at 25%
-            else if((builders.length / Object.keys(totalCreeps).length)<0.25){            
+            else if((builders.length / Object.keys(totalCreeps).length)<=0.25){
                 var newName = 'Builder' + Game.time;
                 console.log('Spawning new Builder: ' + newName);
                 Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
                 {memory: {role: 'builder'}});        
             } // Keeps Upgraders at 25%
-            else if((upgraders.length / Object.keys(totalCreeps).length)<0.25) {
+            else if((upgraders.length / Object.keys(totalCreeps).length)<=0.25) {
                 var newName = 'Upgrader' + Game.time;
                 console.log('Spawning new Upgrader: ' + newName);
                 Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
