affaf8f83e0b6cd12461c91f9c0e8108494393f4
Leon Wilzer <leon.wilzer@protonmail.com>
2021-06-19 22:20:45 +0200

    debug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var manageRole = require('manage.role');
 
 module.exports.loop = function () {
 
+    // Clears creep memory
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    manageRole.run(creep);
+
+    // Display spawing status
+    if(Game.spawns['Spawn1'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1, 
+            Game.spawns['Spawn1'].pos.y, 
+            {align: 'left', opacity: 0.8});
+    }
+
+    // Tower Control
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -19,6 +41,7 @@ module.exports.loop = function () {
         }
     }
 
+    // Creep control
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
---
+++ src/manage.role.js
@@ -0,0 +1,47 @@
+var manageRole = {
+    run: function() {
+        // Contains all Creeps
+        var creepers = Game.creeps;
+        console.log('Total Creeps: ' + Object.keys(creepers).length);
+
+        // Contains all Harvesters
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        console.log('Harvesters: ' + harvesters.length + (harvesters.length / Object.keys(creepers).length) + "%");
+
+        // Contains all Builders
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        console.log('Builders: ' + builders.length + (builders.length / Object.keys(creepers).length) + "%");
+
+        // Contains all Builders
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        console.log('Upgraders: ' + upgraders.length + (harvesters.length / Object.keys(creepers).length) + "%");
+
+        if(Object.keys(creepers).length<10){
+            // Keeps Harvesters at 50%
+            if(!Game.spawns['Spawn1'].spawning && (harvesters.length / Object.keys(creepers).length)<0.5) {
+                var newName = 'Harvester' + Game.time;
+                console.log('Spawning new harvester: ' + newName);
+                Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+                {memory: {role: 'harvester'}});
+            }
+
+            // Keeps Builders at 25%
+            if(!Game.spawns['Spawn1'].spawning && (builders.length / Object.keys(creepers).length)<0.25) {
+                var newName = 'Builder' + Game.time;
+                console.log('Spawning new Builder: ' + newName);
+                Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+                {memory: {role: 'builder'}});        
+            }
+
+            // Keeps Upgraders at 25%
+            if(!Game.spawns['Spawn1'].spawning && (upgraders.length / Object.keys(creepers).length)<0.25) {
+                var newName = 'Upgrader' + Game.time;
+                console.log('Spawning new Upgrader: ' + newName);
+                Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+                {memory: {role: 'upgrader'}});        
+            }
+        }
+    }
+};
+
+module.exports = manageRole;
