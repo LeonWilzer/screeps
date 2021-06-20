ebd9b4afb0b5b0c2b934655b8bf5cf2b4a8f1b52
Leon Wilzer <leon.wilzer@protonmail.com>
2021-06-20 15:29:15 +0200

    debug

--- src/role.builder.js
+++ src/role.builder.js
@@ -18,7 +18,7 @@ var roleBuilder = {
             if(repairs.length)
             {
                 if(creep.repair(repairs[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#f1f509'}});
+                    creep.moveTo(repair[0], {visualizePathStyle: {stroke: '#f1f509'}});
                 }
             }
             else if(targets.length) {
