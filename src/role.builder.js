15bd4255ba391ecc48517784318e94e0ff224fca
Leon Wilzer <leon.wilzer@protonmail.com>
2021-06-20 16:07:37 +0200

    debug

--- src/role.builder.js
+++ src/role.builder.js
@@ -14,7 +14,7 @@ var roleBuilder = {
 
         if(creep.memory.building) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            var repairs = _.filter(creep.room.find(FIND_MY_STRUCTURES), (struct) => (struct.hits/struct.hitsMax)<1);
+            var repairs = _.filter(creep.room.find(FIND_STRUCTURES), (struct) => (struct.hits/struct.hitsMax)<1);
             if(repairs.length)
             {
                 if(creep.repair(repairs[0]) == ERR_NOT_IN_RANGE) {
