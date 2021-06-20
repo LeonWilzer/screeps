a80a26a7710e01ea66eef245cee24f409f52af5a
Leon Wilzer <leon.wilzer@protonmail.com>
2021-06-20 13:48:15 +0200

    debug

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -5,7 +5,7 @@ var roleHarvester = {
         if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             var tombs = creep.room.find(FIND_TOMBSTONES);
-            if(creep.withdraw(tombs[0],RESOURCE_ENERGY == ERR_NOT_IN_RANGE))
+            if(tombs.length > 0 && creep.withdraw(tombs[0],RESOURCE_ENERGY == ERR_NOT_IN_RANGE))
                 creep.moveTo(tombs[0], {visualizePathStyle: {stroke: '#5ec283'}});
             else if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#5ec283'}});
